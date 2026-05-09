fd925e812f5ebfe22f0d2187d4ba8011280c5322
HaiTrieu186 <pnhtrieu186@gmail.com>
2026-05-09 22:32:42 +0700

    Buổi 3: Tạo entity Student và Mentor

---
+++ src/main/java/re/edu/quan_ly_thuc_tap/entity/Student.java
@@ -0,0 +1,50 @@
+package re.edu.quan_ly_thuc_tap.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @Column(name = "student_id")
+    private Long studentId; // Vừa là PK vừa FK
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "student_code", length = 20, unique = true, nullable = false)
+    private String studentCode;
+
+    @Column(name = "major", length = 100)
+    private String major;
+
+    @Column(name = "class", length = 50)
+    private String className;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "address", length = 255)
+    private String address;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
